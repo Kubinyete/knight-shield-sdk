0cc0cad67aa778828bab0a07b27c20d4d842641b
Vitor Kubinyete <vitorkubinyete@hotmail.com>
2023-01-18 08:51:13 -0300

    Updated error messages from ResponseErrorException

--- src/App/Exception/ResponseErrorException.php
+++ src/App/Exception/ResponseErrorException.php
@@ -11,7 +11,11 @@ class ResponseErrorException extends ResponseException
     public function __construct(Response $response)
     {
         $this->response = $response;
-        parent::__construct($this->getResponseMessage() ?? "An API error ocurred.");
+
+        $errorStr = $this->getErrorsString();
+        $errorMessage = $this->getResponseMessage();
+
+        parent::__construct(($errorMessage ?? "An API error ocurred.") . ($errorStr ? ':' . PHP_EOL . $errorStr : ''));
     }
 
     public function getResponseMessage(): ?string
@@ -24,8 +28,22 @@ class ResponseErrorException extends ResponseException
         return $this->response->getStatus();
     }
 
-    public function getErrors(): array
+    public function getErrors(): ?array
     {
         return $this->response->getPath('errors');
+    }
+
+    protected function getErrorsString(): string
+    {
+        $errors = $this->getErrors();
+        $message = '';
+
+        if ($errors) {
+            foreach ($errors as $key => $value) {
+                $message .= "{$key}: " . is_array($value) ? implode(', ', $value) : strval($value) . PHP_EOL;
+            }
+        }
+
+        return $message;
     }
 }
